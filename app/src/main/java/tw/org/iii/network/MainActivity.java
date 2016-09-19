c4e6403e6b23ec23ef5203b1bb4f8a5ff710d56d
justfungx <justfungx@hotmail.com>
2016-09-19 16:41:44 +0800

    V2

--- app/src/main/java/tw/org/iii/network/MainActivity.java
+++ app/src/main/java/tw/org/iii/network/MainActivity.java
@@ -3,12 +3,19 @@ package tw.org.iii.network;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.os.Environment;
+import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.TextView;
 
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
 import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.InetAddress;
 import java.net.NetworkInterface;
@@ -18,10 +25,19 @@ import java.util.Enumeration;
 
 public class MainActivity extends AppCompatActivity {
     private ConnectivityManager mgr;
+    private String data;
+    private TextView mmsg;
+    private StringBuffer sb;
+    private UIHandler handler;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        handler = new UIHandler();
+        mmsg = (TextView)findViewById(R.id.mmsg);
 
         mgr = (ConnectivityManager)getSystemService(CONNECTIVITY_SERVICE);
         NetworkInfo info = mgr.getActiveNetworkInfo();
@@ -36,8 +52,8 @@ public class MainActivity extends AppCompatActivity {
                         Log.d("DK", ia.getHostAddress());
                     }
                 }
-//取得兩組IP
-                //google & Gewy
+
+
             } catch (SocketException e) {
                 e.printStackTrace();
             }
@@ -45,7 +61,9 @@ public class MainActivity extends AppCompatActivity {
             Log.d("DK", "NOT Connect");
         }
 
+
     }
+
     public void test1(View v){
 //        new Thread(){
 //            @Override
@@ -60,12 +78,15 @@ public class MainActivity extends AppCompatActivity {
 //                        sb.append((char)c);
 //                    }
 //                    in.close();
-//                    Log.d("brad", sb.toString());
+//                    Log.d("DK", sb.toString());
 //                }catch(Exception ee){
-//                    Log.d("brad", ee.toString());
+//                    Log.d("DK", ee.toString());
 //                }
 //            }
 //        }.start();
+
+        mmsg.setText("");
+
         MyTread mt1 = new MyTread();
         mt1.start();
     }
@@ -74,20 +95,46 @@ public class MainActivity extends AppCompatActivity {
         @Override
         public void run() {
             try {
-                URL url = new URL("http://www.google.com");
+                URL url = new URL("http://data.coa.gov.tw/Service/OpenData/EzgoTravelFoodStay.aspx");
                 HttpURLConnection conn =  (HttpURLConnection) url.openConnection();
                 conn.connect();
-                InputStream in = conn.getInputStream();
-                int c; StringBuffer sb = new StringBuffer();
-                while ( (c = in.read()) != -1){
-                    sb.append((char)c);
-                }
-                in.close();
-                Log.d("DK", sb.toString());
+                BufferedReader reader =
+                        new BufferedReader(new InputStreamReader(conn.getInputStream()));
+                data = reader.readLine();
+                reader.close();
+                parseJSON();
             }catch(Exception ee){
                 Log.d("DK", ee.toString());
             }
         }
+    }
+
+    private void parseJSON(){
+        sb = new StringBuffer();
+        try {
+            JSONArray root = new JSONArray(data);
+            for (int i=0; i<root.length(); i++){
+                JSONObject row = root.getJSONObject(i);
+                String name = row.getString("Name");
+                String add = row.getString("Address");
+                Log.d("DK", name + " -> " + add);
+                sb.append(name + " -> " + add + "\n");
+            }
+            handler.sendEmptyMessage(0);
+        }catch(Exception ee){
+            Log.d("DK", ee.toString());
+        }
+    }
+
+    private class UIHandler extends android.os.Handler {
+        @Override
+        public void handleMessage(Message msg) {
+            super.handleMessage(msg);
+            mmsg.setText(sb);
+
+
+        }
+
     }
 
 }
